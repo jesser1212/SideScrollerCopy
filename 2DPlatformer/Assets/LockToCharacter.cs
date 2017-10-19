06a551984d7c7d8eef3aaecd2c0a40c6d94c946b
Jesse Robinson <jesser1212@gmail.com>
2017-10-19 14:16:38 -0600

    CopiedProject
    Copied the side scroller with changes.

---
+++ 2DPlatformer/Assets/LockToCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockToCharacter : MonoBehaviour {
+
+	public GameObject Character;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Character.transform.position;
+		pos.z = transform.position.z;
+		gameObject.transform.position = pos;
+	}
+}
---
+++ 2DPlatformer/Assets/Tutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+
+	public int lives = 0;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (ShowVictory ());
+		Vector3 vec = transform.position;
+		vec.x = 5;
+		transform.position = vec;
+	}//Queue 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator ShowVictory(){
+		print ("Hello");
+		yield return new WaitForSeconds (3);
+		Debug.Log ("Hi");
+		lives = 100;
+	}
+}
